ed1ffe4b5fd1656e33d605d3f1d6f426770f4ff8
Alejandro Cruz Valera <alex.c.valera94@hotmail.com>
2020-10-18 12:17:24 -0500

    ÂAdded info sectiosn with styled components

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,56 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDescription: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description:
+        'Get access  to our  exclusive  app that  allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    dark2: true,
+    primary: true,
+    darkText: false,
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDescription: false,
+    topLine: 'Unlimited Access',
+    headLine: 'Unlimited Transactions with zero fees',
+    description:
+        'Get access  to our  exclusive  app that  allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Pegy',
+    dark: false,
+    dark2: true,
+    primary: false,
+    darkText: true,
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDescription: false,
+    topLine: 'Join our team',
+    headLine: 'Creating an account is extremely easy',
+    description:
+        'Get everything set up and ready in under 10 minutes. All you need to do is add your information and you´re information ready to go.',
+    buttonLabel: 'Start now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Car',
+    dark: false,
+    dark2: true,
+    primary: false,
+    darkText: true,
+};
